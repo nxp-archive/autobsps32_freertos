0c34383db26380d2750d148db89cd4373a5eec96
Madalina Hristache-B55105 <madalina.hristache@freescale.com>
2015-07-23 10:02:10 +0300

    Update linker script for GCC and FreeRTOSConfig.h
    The updates are made to run the application from RAM, instead of FLASH.
    
    Signed-off-by: Madalina Hristache-B55105 <madalina.hristache@freescale.com>

--- FreeRTOS/Demo/CORTEX_M4F_S32K_GCC_IAR/FreeRTOSConfig.h
+++ FreeRTOS/Demo/CORTEX_M4F_S32K_GCC_IAR/FreeRTOSConfig.h
@@ -48,9 +48,9 @@
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 5 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 90 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 30 * 1024 ) )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 8 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 10 )
-#define configUSE_TRACE_FACILITY		1
+#define configUSE_TRACE_FACILITY		0
 #define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			1
 #define configUSE_MUTEXES				1
@@ -101,7 +101,7 @@ FreeRTOS/Source/tasks.c for limitations. */
 	void vMainConfigureTimerForRunTimeStats( void );
 	unsigned long ulMainGetRunTimeCounterValue( void );
 #endif
-#define configGENERATE_RUN_TIME_STATS	1
+#define configGENERATE_RUN_TIME_STATS	0
 #define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS() vMainConfigureTimerForRunTimeStats()
 #define portGET_RUN_TIME_COUNTER_VALUE() ulMainGetRunTimeCounterValue()
 
