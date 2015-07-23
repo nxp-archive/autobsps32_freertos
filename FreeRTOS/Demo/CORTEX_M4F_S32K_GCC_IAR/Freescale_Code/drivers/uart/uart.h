b6417e1f459c6c572e42c8598baaa5ff23e6a346
Madalina Hristache-B55105 <madalina.hristache@freescale.com>
2015-07-23 10:50:24 +0300

    FREERTOS-4 Update memory layer and register definitions for uart
    Signed-off-by: Madalina Hristache-B55105 <madalina.hristache@freescale.com>

--- FreeRTOS/Demo/CORTEX_M4F_S32K_GCC_IAR/Freescale_Code/drivers/uart/uart.h
+++ FreeRTOS/Demo/CORTEX_M4F_S32K_GCC_IAR/Freescale_Code/drivers/uart/uart.h
@@ -1,6 +1,6 @@
 /*
  * File:		uart.h
- * Purpose:     Provide common ColdFire UART routines for polled serial IO
+ * Purpose:		Provide common ColdFire UART routines for polled serial IO
  *
  * Notes:
  */
@@ -10,10 +10,10 @@
 
 /********************************************************************/
 
-void uart_init (UART_MemMapPtr, int, int);
-char uart_getchar (UART_MemMapPtr);
-void uart_putchar (UART_MemMapPtr, char);
-int uart_getchar_present (UART_MemMapPtr);
+void uart_init (LPUART_MemMapPtr, int, int);
+char uart_getchar (LPUART_MemMapPtr);
+void uart_putchar (LPUART_MemMapPtr, char);
+int uart_getchar_present (LPUART_MemMapPtr);
 
 /********************************************************************/
 
