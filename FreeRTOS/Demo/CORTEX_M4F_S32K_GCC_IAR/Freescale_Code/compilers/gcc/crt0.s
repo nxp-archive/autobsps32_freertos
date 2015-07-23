5901d206981158dd9e3eed75ef721e577ab62c67
Madalina Hristache-B55105 <madalina.hristache@freescale.com>
2015-07-23 11:01:50 +0300

    FREERTOS-5 Modify startup code to place the interrupt vector in RAM
    Signed-off-by: Madalina Hristache-B55105 <madalina.hristache@freescale.com>

--- FreeRTOS/Demo/CORTEX_M4F_S32K_GCC_IAR/Freescale_Code/compilers/gcc/crt0.s
+++ FreeRTOS/Demo/CORTEX_M4F_S32K_GCC_IAR/Freescale_Code/compilers/gcc/crt0.s
@@ -16,6 +16,7 @@
 .equiv VTOR_REG, 0xE000ED08
 
 .align 2
+
 .globl __startup
 .type __startup, %function
 
@@ -34,6 +35,15 @@ __startup:
 	mov     r10,#0
 	mov     r11,#0
 	mov     r12,#0
+	/* Setup only for FPGA */
+	ldr     r13, =__BOOT_STACK_ADDRESS
+
+	/* Relocate vector table to RAM */
+	ldr  r0, =VTOR_REG
+	ldr  r1, =__vector_table
+	/* ldr  r2, =(1 << 29) */
+	/* orr  r1, r2  r1 = r1 | r2 */
+	str  r1,[r0]
 
 	bl      start                  /* call the C code */
 
