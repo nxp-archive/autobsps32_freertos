66449db66f672d98a14d67ae11aaba6240e0ba0c
Madalina Hristache-B55105 <madalina.hristache@freescale.com>
2015-07-23 10:50:24 +0300

    FREERTOS-4 Update memory layer and register definitions for uart
    Signed-off-by: Madalina Hristache-B55105 <madalina.hristache@freescale.com>

--- FreeRTOS/Demo/CORTEX_M4F_S32K_GCC_IAR/Freescale_Code/drivers/uart/uart.c
+++ FreeRTOS/Demo/CORTEX_M4F_S32K_GCC_IAR/Freescale_Code/drivers/uart/uart.c
@@ -2,8 +2,8 @@
  * File:        uart.c
  * Purpose:     Provide common UART routines for serial IO
  *
- * Notes:       
- *              
+ * Notes:
+ *
  */
 
 #include "common.h"
@@ -23,107 +23,145 @@
  *  sysclk      UART module Clock in kHz(used to calculate baud)
  *  baud        UART baud rate
  */
-void uart_init (UART_MemMapPtr uartch, int sysclk, int baud)
+void uart_init (LPUART_MemMapPtr uartch, int sysclk, int baud)
 {
-    register uint16 sbr, brfa;
-    uint8 temp;
-    
-	/* Enable the clock to the selected UART */    
-    if(uartch == UART0_BASE_PTR)
-		SIM_SCGC4 |= SIM_SCGC4_UART0_MASK;
-    else
-    	if (uartch == UART1_BASE_PTR)
-			SIM_SCGC4 |= SIM_SCGC4_UART1_MASK;
-    	else
-    		if (uartch == UART2_BASE_PTR)
-    			SIM_SCGC4 |= SIM_SCGC4_UART2_MASK;
-    		else
-    			if(uartch == UART3_BASE_PTR)
-    				SIM_SCGC4 |= SIM_SCGC4_UART3_MASK;
-    			else
-    				if(uartch == UART4_BASE_PTR)
-    					SIM_SCGC1 |= SIM_SCGC1_UART4_MASK;
-    				else
-    					SIM_SCGC1 |= SIM_SCGC1_UART5_MASK;
-                                
-    /* Make sure that the transmitter and receiver are disabled while we 
-     * change settings.
-     */
-    UART_C2_REG(uartch) &= ~(UART_C2_TE_MASK
-				| UART_C2_RE_MASK );
+	register uint16 sbr, sbrTemp;
+	uint16 i;
+	uint32 osr, tempDiff, calculatedBaud, baudDiff, sourceClockInHz;
 
-    /* Configure the UART for 8-bit mode, no parity */
-    UART_C1_REG(uartch) = 0;	/* We need all default settings, so entire register is cleared */
-    
-    /* Calculate baud settings */
-    sbr = (uint16)((sysclk*1000)/(baud * 16));
-        
-    /* Save off the current value of the UARTx_BDH except for the SBR field */
-    temp = UART_BDH_REG(uartch) & ~(UART_BDH_SBR(0x1F));
-    
-    UART_BDH_REG(uartch) = temp |  UART_BDH_SBR(((sbr & 0x1F00) >> 8));
-    UART_BDL_REG(uartch) = (uint8)(sbr & UART_BDL_SBR_MASK);
-    
-    /* Determine if a fractional divider is needed to get closer to the baud rate */
-    brfa = (((sysclk*32000)/(baud * 16)) - (sbr * 32));
-    
-    /* Save off the current value of the UARTx_C4 register except for the BRFA field */
-    temp = UART_C4_REG(uartch) & ~(UART_C4_BRFA(0x1F));
-    
-    UART_C4_REG(uartch) = temp |  UART_C4_BRFA(brfa);    
+	/* Enable the clock to the selected UART */
+	if(uartch == LPUART0_BASE_PTR)
+		PCC_LPUART0 |= PCC_LPUART0_CGC_MASK;
+	else
+		if (uartch == LPUART1_BASE_PTR)
+			PCC_LPUART1 |= PCC_LPUART1_CGC_MASK;
+		else
+			if (uartch == LPUART2_BASE_PTR)
+				PCC_LPUART2 |= PCC_LPUART2_CGC_MASK;
+			else
+				PCC_LPUART3 |= PCC_LPUART3_CGC_MASK;
 
-    /* Enable receiver and transmitter */
-	UART_C2_REG(uartch) |= (UART_C2_TE_MASK
-				| UART_C2_RE_MASK );
+	/* Make sure that the transmitter and receiver are disabled while we
+	 * change settings.
+	 */
+	LPUART_CTRL_REG(uartch) &= ~( LPUART_CTRL_TE_MASK | LPUART_CTRL_RE_MASK );
+
+	/* Configure the UART for 8-bit mode, no parity */
+	LPUART_CTRL_REG(uartch) = 0;	/* We need all default settings, so entire register is cleared */
+
+	sourceClockInHz = sysclk * 1000;
+
+	/* This lpuart instantiation uses a slightly different baud rate calculation
+	 * The idea is to use the best OSR (over-sampling rate) possible
+	 * Note, osr is typically hard-set to 16 in other lpuart instantiations
+	 * First calculate the baud rate using the minimum OSR possible (4) */
+	osr = 4;
+	sbr = (sourceClockInHz/(baud * osr));
+	calculatedBaud = (sourceClockInHz / (osr * sbr));
+
+	if (calculatedBaud > baud)
+	{
+		baudDiff = calculatedBaud - baud;
+	}
+	else
+	{
+		baudDiff = baud - calculatedBaud;
+	}
+
+	/* Loop to find the best osr value possible, one that generates minimum baudDiff
+	 * Iterate through the rest of the supported values of osr */
+	for (i = 5; i <= 32; i++)
+	{
+		/* Calculate the temporary sbr value   */
+		sbrTemp = (sourceClockInHz/(baud * i));
+		/* Calculate the baud rate on the temporary osr and sbr values */
+		calculatedBaud = (sourceClockInHz / (i * sbrTemp));
+
+		if (calculatedBaud > baud)
+		{
+			tempDiff = calculatedBaud - baud;
+		}
+		else
+		{
+			tempDiff = baud - calculatedBaud;
+		}
+
+		if (tempDiff <= baudDiff)
+		{
+			baudDiff = tempDiff;
+			osr = i;  /* Update and store the best osr value calculated */
+			sbr = sbrTemp;  /* Update store the best sbr value calculated */
+		}
+	}
+
+	/* Check to see if actual baud rate is within 3% of desired baud rate
+	 * on the best calculate osr value */
+	if (baudDiff < ((baud / 100) * 3))
+	{
+		/* Acceptable baud rate, check if osr is between 4x and 7x oversampling.
+		 * If so, then "BOTHEDGE" sampling must be turned on */
+		if ((osr > 3) && (osr < 8))
+		{
+			LPUART_BWR_BAUD_BOTHEDGE(uartch, 1);
+		}
+
+		/* Program the osr value (bit value is one less than actual value) */
+		LPUART_BWR_BAUD_OSR(uartch, (osr-1));
+
+		/* Write the sbr value to the BAUD registers */
+		LPUART_BWR_BAUD_SBR(uartch, sbr);
+	}
+
+	/* Enable receiver and transmitter */
+	LPUART_CTRL_REG(uartch) |= ( LPUART_CTRL_TE_MASK | LPUART_CTRL_RE_MASK );
 }
 /********************************************************************/
 /*
  * Wait for a character to be received on the specified UART
  *
  * Parameters:
- *  channel      UART channel to read from
+ *  uartch      UART channel to read from
  *
  * Return Values:
  *  the received character
  */
-char uart_getchar (UART_MemMapPtr channel)
+char uart_getchar (LPUART_MemMapPtr uartch)
 {
-    /* Wait until character has been received */
-    while (!(UART_S1_REG(channel) & UART_S1_RDRF_MASK));
-    
-    /* Return the 8-bit data from the receiver */
-    return UART_D_REG(channel);
+	/* Wait until character has been received */
+	while (!(LPUART_STAT_REG(uartch) & LPUART_STAT_RDRF_MASK));
+
+	/* Return the 8-bit data from the receiver */
+	return LPUART_DATA_REG(uartch);
 }
 /********************************************************************/
 /*
  * Wait for space in the UART Tx FIFO and then send a character
  *
  * Parameters:
- *  channel      UART channel to send to
- *  ch			 character to send
- */ 
-void uart_putchar (UART_MemMapPtr channel, char ch)
+ *  uartch       UART channel to send to
+ *  ch           character to send
+ */
+void uart_putchar (LPUART_MemMapPtr uartch, char ch)
 {
-    /* Wait until space is available in the FIFO */
-    while(!(UART_S1_REG(channel) & UART_S1_TDRE_MASK));
-    
-    /* Send the character */
-    UART_D_REG(channel) = (uint8)ch;
- }
+	/* Wait until space is available in the FIFO */
+	while(!(LPUART_STAT_REG(uartch) & LPUART_STAT_TDRE_MASK));
+
+	/* Send the character */
+	LPUART_WR_DATA(uartch, (uint8)ch);
+}
 /********************************************************************/
 /*
  * Check to see if a character has been received
  *
  * Parameters:
- *  channel      UART channel to check for a character
+ *  uartch      UART channel to check for a character
  *
  * Return values:
  *  0       No character received
  *  1       Character has been received
  */
-int uart_getchar_present (UART_MemMapPtr channel)
+int uart_getchar_present (LPUART_MemMapPtr uartch)
 {
-    return (UART_S1_REG(channel) & UART_S1_RDRF_MASK);
+	return (LPUART_STAT_REG(uartch) & LPUART_STAT_RDRF_MASK);
 }
 /********************************************************************/
-    
