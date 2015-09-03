4aecb149023edca095a05750ee56863ab716ca35
Madalina Hristache-B55105 <madalina.hristache@freescale.com>
2015-09-03 17:25:14 +0300

    Remove useless flash_identify function
    Signed-off-by: Madalina Hristache-B55105 <madalina.hristache@freescale.com>

--- FreeRTOS/Demo/CORTEX_M4F_S32K_GCC_IAR/Freescale_Code/cpu/start.c
+++ FreeRTOS/Demo/CORTEX_M4F_S32K_GCC_IAR/Freescale_Code/cpu/start.c
@@ -15,7 +15,7 @@
  * \brief   Kinetis Start
  * \return  None
  *
- * This function calls all of the needed starup routines and then
+ * This function calls all of the needed startup routines and then
  * branches to the main process.
  */
 void start(void)
@@ -29,57 +29,10 @@ void start(void)
 	/* Perform processor initialization */
 	sysinit();
 
-	/* Determine the flash revision */
-	flash_identify();
-
 	/* Jump to main process */
 	main();
 
 	/* No actions to perform after this so wait forever */
 	while(1);
-}
-/********************************************************************/
-/*!
- * \brief   flash Identify
- * \return  None
- *
- * This is primarly a reporting function that displays information
- * about the specific flash parameters and flash version ID for
- * the current device. These parameters are obtained using a special
- * flash command call "read resource." The first four bytes returned
- * are the flash parameter revision, and the second four bytes are
- * the flash version ID.
- */
-void flash_identify (void)
-{
-    /* Get the flash parameter version */
-
-	/* Write the flash FCCOB registers with the values for a read resource command */
-	FTFE_FCCOB0 = 0x03;
-	FTFE_FCCOB1 = 0x00;
-	FTFE_FCCOB2 = 0x00;
-	FTFE_FCCOB3 = 0x00;
-	FTFE_FCCOB8 = 0x01;
-
-	/* All required FCCOBx registers are written, so launch the command */
-	FTFE_FSTAT = FTFE_FSTAT_CCIF_MASK;
-
-	/* Wait for the command to complete */
-	while(!(FTFE_FSTAT & FTFE_FSTAT_CCIF_MASK));
-
-	/* Get the flash version ID */
-
-	/* Write the flash FCCOB registers with the values for a read resource command */
-	FTFE_FCCOB0 = 0x03;
-	FTFE_FCCOB1 = 0x00;
-	FTFE_FCCOB2 = 0x00;
-	FTFE_FCCOB3 = 0x04;
-	FTFE_FCCOB8 = 0x01;
-
-	/* All required FCCOBx registers are written, so launch the command */
-	FTFE_FSTAT = FTFE_FSTAT_CCIF_MASK;
-
-	/* Wait for the command to complete */
-	while(!(FTFE_FSTAT & FTFE_FSTAT_CCIF_MASK));
 }
 /********************************************************************/
