048823a50e50a7e42a12db4c0605baf07714a729
Anh Tuan Nguyen <anhtuan.nguyen@nxp.com>
2018-08-09 13:55:14 +0700

    Add support GCC compiler and rename directory FreeRTOS/Demo/CORTEX_Kinetis_K60_Tower_IAR to FreeRTOS/Demo/CORTEX_Kinetis_K60_Tower_GCC_IAR
    Signed-off-by: Anh Tuan Nguyen <anhtuan.nguyen@nxp.com>

---
+++ FreeRTOS/Demo/CORTEX_Kinetis_K60_Tower_GCC_IAR/Freescale_Code/common/common.h
@@ -0,0 +1,69 @@
+/*
+ * File:        common.h
+ * Purpose:     File to be included by all project files
+ *
+ * Notes:
+ */
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+/********************************************************************/
+
+/*
+ * Debug prints ON (#define) or OFF (#undef)
+ */
+#define DEBUG
+#define DEBUG_PRINT
+
+/*
+ * Include the generic CPU header file
+ */
+#include "arm_cm4.h"
+
+/*
+ * Include the platform specific header file
+ */
+#if (defined(TWR_K60N512))
+	#include "k60_tower.h"
+#else
+	#error "No valid platform defined"
+#endif
+
+/*
+ * Include the cpu specific header file
+ */
+#if (defined(CPU_MK60N512VMD100))
+	#include <MK60N512VMD100.h>
+#else
+	#error "No valid CPU defined"
+#endif
+
+/*
+ * Include any toolchain specfic header files
+ */
+#if (defined(CW))
+	#include "cw.h"
+#elif (defined(IAR))
+	#include "iar.h"
+#elif (defined(GCC))
+	#include "gcc.h"
+#else
+	#warning "No toolchain specific header included"
+#endif
+
+/*
+ * Include common utilities
+ */
+#include "assert.h"
+#include "io.h"
+#include "startup.h"
+#include "stdlib.h"
+
+#if (defined(IAR))
+	#include "intrinsics.h"
+#endif
+
+/********************************************************************/
+
+#endif /* _COMMON_H_ */
