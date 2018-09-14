7983f5f6410eda453ffbd1429281acb6eb6726dc
Huynh Vu Tien <tien.huynhvu@nxp.com>
2018-09-14 14:41:07 +0700

    [Sample] Modify FreeRTOSConfig.h for S32V234
    	- Modify FreeRTOSConfig.h for this main application
    
    Issue: FREERTOS-97
    
    Signed-off-by: Huynh Vu Tien <tien.huynhvu@nxp.com>

--- FreeRTOS/Demo/S32V_GCC/SOURCES/FreeRTOSConfig.h
+++ FreeRTOS/Demo/S32V_GCC/SOURCES/FreeRTOSConfig.h
@@ -44,11 +44,11 @@
 #define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK				1
 #define configUSE_TICK_HOOK				1
-#define configCPU_CLOCK_HZ				( 48000000UL )
+#define configCPU_CLOCK_HZ				( 20000000UL )
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 5 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 90 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 5 * 1024 ) )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 8 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 10 )
 #define configUSE_TRACE_FACILITY		0
 #define configUSE_16_BIT_TICKS			0
