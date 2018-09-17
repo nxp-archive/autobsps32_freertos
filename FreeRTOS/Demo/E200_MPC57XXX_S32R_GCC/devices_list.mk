3b6c01f0223f09b6fbf855a3e3a3b20dd3caa104
Chung Nguyen Thanh <chung.nguyenthanh@nxp.com>
2018-09-17 10:32:15 +0700

    add define mpc5744p platform into devices_list.mk
    Issue: FREERTOS-99
    
    Signed-off-by: Chung Nguyen Thanh <chung.nguyenthanh@nxp.com>

--- FreeRTOS/Demo/E200_MPC57XXX_S32R_GCC/devices_list.mk
+++ FreeRTOS/Demo/E200_MPC57XXX_S32R_GCC/devices_list.mk
@@ -3,7 +3,7 @@
 #
 
 # list MCU supported
-mcu_list := mpc5746c mpc5748g s32r274 s32r372
+mcu_list := mpc5744p mpc5746c mpc5748g s32r274 s32r372
 
 ifeq ($(filter $(MCU),$(mcu_list)),)
     $(error Undefined MCU: '$(MCU)', use one from: $(mcu_list))
