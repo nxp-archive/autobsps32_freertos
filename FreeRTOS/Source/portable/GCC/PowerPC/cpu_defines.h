3d1056a0760c862f84308b01037bb473236eb891
Chung Nguyen Thanh <chung.nguyenthanh@nxp.com>
2018-09-18 10:30:26 +0700

    Bring back FreeRTOS/Source/portable/GCC/PowerPC from 9410b1f1fedbc396eff1a406bc1fdf79e322a2df
    Issue: FREERTOS-101
    
    Signed-off-by: Chung Nguyen Thanh <chung.nguyenthanh@nxp.com>

---
+++ FreeRTOS/Source/portable/GCC/PowerPC/cpu_defines.h
@@ -0,0 +1,30 @@
+/*
+*   Copyright 2018 NXP.
+*/
+
+#ifndef CPU_DEFINES_H
+#define CPU_DEFINES_H
+
+#if defined(CPU_MPC5746C) || defined(CPU_MPC5748G) || defined(CPU_MPC5744P) || defined(CPU_S32R274) || defined(CPU_S32R372) || defined(CPU_MPC5746R)
+    #define INTC_CPR_ADDR_BASE                      0xFC040010
+    #define INTC_IACKR_PRC_ADDR_BASE                0xFC040020
+    #define INTC_EOIR_PRC_ADDR_BASE                 0xFC040030
+    #define INTC_IACKR_INTVEC_BITWIDTH_NUM_BASE     10
+    #define INTC_OFFSET_NUM                            2
+#elif defined(CPU_MPC5777C)
+    #define INTC_CPR_ADDR_BASE                      0xFFF48008
+    #define INTC_IACKR_PRC_ADDR_BASE                0xFFF48010
+    #define INTC_EOIR_PRC_ADDR_BASE                 0xFFF48018
+    #define INTC_IACKR_INTVEC_BITWIDTH_NUM_BASE     10
+    #define INTC_OFFSET_NUM                            2
+#else
+    #error not define platform
+#endif
+
+    #define INTC_OFFSET(coreId)                        (coreId << INTC_OFFSET_NUM)
+    #define INTC_CPR_ADDR(coreId)                      INTC_CPR_ADDR_BASE + INTC_OFFSET((coreId))
+    #define INTC_IACKR_PRC_ADDR(coreId)                INTC_IACKR_PRC_ADDR_BASE + INTC_OFFSET((coreId))
+    #define INTC_EOIR_PRC_ADDR(coreId)                 INTC_EOIR_PRC_ADDR_BASE + INTC_OFFSET((coreId))
+    #define INTC_IACKR_INTVEC_BITWIDTH_NUM             INTC_IACKR_INTVEC_BITWIDTH_NUM_BASE
+
+#endif/* CPU_DEFINES_H */
