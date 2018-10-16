3bf0dff1427a9b8c6927d0098bb89b46083ed6f0
Huynh Vu Tien <tien.huynhvu@nxp.com>
2018-10-16 15:05:02 +0700

    [Sample] Create source hardware for S32V234 Platform
    	- Create hardware source code to support for specific hardware
    
    Issue: FREERTOS-97
    Signed-off-by: Huynh Vu Tien <tien.huynhvu@nxp.com>

---
+++ FreeRTOS/Demo/S32V_GCC/SOURCES_HW/sysinit.h
@@ -0,0 +1,15 @@
+/*
+ * File:        sysinit.h
+ * Purpose:     S32V234 Configuration
+ *
+ * Notes:
+ *
+ */
+
+/********************************************************************/
+
+/* Function prototypes */
+extern void sysinit (void);
+extern void common_startup (void);
+
+/********************************************************************/
