313a721d840a0b5313748f95ee17b336558dc0ad
Chung Nguyen Thanh <chung.nguyenthanh@nxp.com>
2018-12-14 10:42:45 +0700

    Add MIT header into all source of MPC5777C platform
    Issue: FREERTOS-124
    
    Signed-off-by: Chung Nguyen Thanh <chung.nguyenthanh@nxp.com>

--- FreeRTOS/Demo/E200_MPC57XXX_S32R_GCC/specific/mpc5777c/inc/hw_intc.h
+++ FreeRTOS/Demo/E200_MPC57XXX_S32R_GCC/specific/mpc5777c/inc/hw_intc.h
@@ -1,6 +1,26 @@
-/*
-*   Copyright 2018 NXP
-*/
+/*-----------------------------------------------------------
+ *
+ *  Copyright 2018 NXP
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/
+ * or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ *----------------------------------------------------------*/
 
 #ifndef HW_INTC_H
 #define HW_INTC_H
@@ -26,7 +46,7 @@ extern "C"
     #define HWINTC_SOFTCLEAR     (0x1U)                                               /* bit number to clear setable software interrupt */
 
     #define HWINTC_PSR_ADDR(intr) (HWINTC_BASE_ADDR + 0x40U + (intr))  /* address of INTC_PSR for intr input */
-	
+
     #define HWINTC_PSR(intr)            HWREG8( HWINTC_PSR_ADDR(intr) )                    /* INTC_PSR register for intr input */
     #define HWINTC_PRIO(intr)           HWREG8( HWINTC_PSR_ADDR(intr) )                /* Priority select register for intr input */
     #define HWINTC_SET_PRC_SEL(coreId)          (unsigned char)((coreId*3) << 6)                            /* Processor select bits' registor for intr input */
