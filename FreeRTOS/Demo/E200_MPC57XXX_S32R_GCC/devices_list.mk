a0d7f581f378bcd9c47a27321d88ee06ea1ede3e
Phuoc Le Dang <phuoc.ledang@nxp.com>
2020-04-03 16:51:08 +0700

    Update source code base to support s32r294 platform
    Issue: FREERTOS-189
    Signed-off-by: Phuoc Le Dang <phuoc.ledang@nxp.com>

--- FreeRTOS/Demo/E200_MPC57XXX_S32R_GCC/devices_list.mk
+++ FreeRTOS/Demo/E200_MPC57XXX_S32R_GCC/devices_list.mk
@@ -3,7 +3,7 @@
 #
 
 # list MCU supported
-mcu_list := s32r274 s32r372 s32r264
+mcu_list := s32r274 s32r372 s32r264 s32r294
 
 ifeq ($(filter $(MCU),$(mcu_list)),)
     $(error Undefined MCU: '$(MCU)', use one from: $(mcu_list))
